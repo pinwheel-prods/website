53eb01a3998e08d44b97066e5bfe07dffbc30a19
Leila Business <leilabusiness@Khaleds-MacBook-Campus.local>
2020-04-23 13:25:37 -0400

    sign in/up

---
+++ js/pinwheel-bios/firebase-config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCdBeKOhYBhC1VPmkZ0GXhScbFRJFD0-r4",
+  authDomain: "pinwheel-productions.firebaseapp.com",
+  databaseURL: "https://pinwheel-productions.firebaseio.com",
+  projectId: "pinwheel-productions",
+  storageBucket: "pinwheel-productions.appspot.com",
+  messagingSenderId: "205414102112",
+  appId: "1:205414102112:web:f272953210cfbdacb03f05",
+  measurementId: "G-JWMEWTWLMJ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
